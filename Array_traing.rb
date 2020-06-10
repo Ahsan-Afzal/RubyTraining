6c7c9e394d6c019adaa98c6bd00430b854f73dd8
M.Ahsan <ahsan.afzal@pakeventures.com>
2020-06-10 13:01:40 +0500

    array creation methods

---
+++ Array_traing.rb
@@ -0,0 +1,16 @@
+# array training
+# creating arrays
+i_array = [1, 2, 3, 4, 5, 6, 7]
+str_array = %w[abc def ghi]
+mix_array = [i_array, str_array]
+
+ar = Array.new()
+ar.push([1, 2, 3, 4, 5])
+ar = Array.new(3)
+ar = Array.new(3, 5)
+
+
+
+print ar
+
+
